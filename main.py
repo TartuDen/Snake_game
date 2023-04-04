07caa7af5e161b8d91187b132b285db343ffb081
Pomogadryg <denver1033@gmail.com>
2023-04-04 15:35:40 +0300

    copied files from another directory, now this folder has main files

--- main.py
+++ main.py
@@ -1,57 +1,78 @@
-# from turtle import Screen, Turtle
+from snake import Snake
+from food import Food
 import turtle
+from food import Food
+
+
 
 screen=turtle.Screen()
-screen.setup(width=600, height=600)
+set_width=600
+set_height=600
+screen.setup(width=set_width, height=set_height)
 screen.bgcolor("black")
 screen.title("Python Snake")
+food_=Food()
+
+
+
 turtle.listen()
+
 
 
 def create_snake(length=3):
     temp=[]
+    start_pos=(0-len(temp)*20,0)
     for i in range(length):
         s=turtle.Turtle()
         s.penup()
         s.shape("square")
         s.color("white")
-        s.goto(0-len(temp)*20,0)
+        s.goto(start_pos)
         temp.append(s)
     return(temp)
 
-snake=(create_snake())
+snake_list=(create_snake())
+
+def update_snake(pos):
+    global snake_list
+    temp=[]
+    for i in snake_list:
+        temp.append(i)
+    s=turtle.Turtle()
+    s.penup()
+    s.shape("square")
+    s.color("white")
+    s.goto(pos)
+    temp.append(s)
+    
+    snake_list=temp
+    print(".......... the  snake is updated", snake_list)
 
 
-def move_forward():
-    pos_list=[]
-    for idx,s in enumerate(snake):
-        pos=s.position()
-        pos_list.append(pos)
-        if idx==0:
-            s.goto(pos[0]+20,pos[1])
-        else:
-            pos=pos_list[idx-1]
-            s.goto(pos)
-        print(idx, s.position())
-    print("pos_list: ",pos_list)
 
 
 
-def move_up():
-    pass
-
-
-# def move_down():
-#     for each_block in (snake):
-#         each_block.forward(10)
-# def move_forward():
-#     for each_block in (snake):
-#         each_block.forward(10)
-
-turtle.onkey(move_forward, "Right")
-# turtle.onkey(move_up, "Up")
-# turtle.onkey(move_down, "Left")
-# turtle.onkey(move_forward, "Right")
-
+while True:
+    snake_=Snake(set_width,set_height,snake_list,food_)
+    resp_from_snake=snake_.move_constant()
+    if resp_from_snake == "wall":
+        print("GAME OVER!")
+        break
+    elif resp_from_snake==False: 
+        screen.update()
+        turtle.onkey(snake_.move_forward, "Right")
+        turtle.onkey(snake_.move_up, "Up")
+        turtle.onkey(snake_.move_down, "Down")
+        turtle.onkey(snake_.move_backwards, "Left")
+    else:
+        print(".......... we found something")
+        food_.food.hideturtle()
+        food_.food.clear()
+        food_=Food()
+        update_snake(resp_from_snake)
 
 screen.exitonclick()
+
+
+
+    
---
+++ snake.py
@@ -0,0 +1,131 @@
+import math
+import turtle
+import time
+from food import Food
+
+class Snake():
+    def __init__(self,set_width,set_height,snake,food):
+        self.set_width=set_width
+        self.set_height=set_height
+        self.food=food
+        print(self.food.food.position())
+
+        self.snake=snake  
+    
+    def move_forward(self):
+        pos_list=[]
+        for idx,s in enumerate(self.snake):
+            pos=s.position()
+            pos_list.append(pos)
+            if idx==0:
+                heading=s.heading()
+                if int(heading)==90:
+                    s.right(90)
+                elif int(heading)==270:
+                    s.left(90)
+                s.goto(pos[0]+20,pos[1])
+            else:
+                pos=pos_list[idx-1]
+                s.goto(pos)
+
+
+    def move_up(self):
+        pos_list=[]
+        for idx,s in enumerate(self.snake):
+            pos=s.position()
+            pos_list.append(pos)
+            if idx==0:
+                heading=s.heading()
+                if int(heading)==0:
+                    s.left(90)
+                elif int(heading)==180:
+                    s.right(90)
+                s.goto(pos[0],pos[1]+20)
+            else:
+                pos=pos_list[idx-1]
+                s.goto(pos)
+
+
+    def move_down(self):
+        pos_list=[]
+        for idx,s in enumerate(self.snake):
+            pos=s.position()
+            pos_list.append(pos)
+            if idx==0:
+                heading=s.heading()
+                if int(heading)==0:
+                    s.right(90)
+                elif int(heading)==180:
+                    s.left(90)
+                s.goto(pos[0],pos[1]-20)
+            else:
+                pos=pos_list[idx-1]
+                s.goto(pos)
+
+
+    def move_backwards(self):
+        pos_list=[]
+        for idx,s in enumerate(self.snake):
+            pos=s.position()
+            pos_list.append(pos)
+            if idx==0:
+                heading=s.heading()
+                if int(heading)==90:
+                    s.left(90)
+                elif int(heading)==270:
+                    s.right(90)
+                s.goto(pos[0]-20,pos[1])
+            else:
+                pos=pos_list[idx-1]
+                s.goto(pos)
+
+
+    def move_constant(self):
+        speed=0.0
+        pos_list=[]
+        for idx,s in enumerate(self.snake):
+            time.sleep(speed)
+            pos=s.position()
+            pos_list.append(pos)
+            if idx==0:
+                heading = s.heading()
+                if heading == 0:
+                    s.goto(pos[0]+20,pos[1])
+                elif heading == 180:
+                    s.goto(pos[0]-20,pos[1])
+                elif heading == 90:
+                    s.goto(pos[0],pos[1]+20)
+                elif heading == 270:
+                    s.goto(pos[0],pos[1]-20)
+                if self.hit_the_wall(pos):
+                    return("wall")
+                if self.consume_the_food(pos):
+                    return(pos)
+            else:
+                pos=pos_list[idx-1]
+                s.speed("fastest")
+                s.goto(pos)
+        return(False)
+    
+    def consume_the_food(self, pos):
+        x,y=self.food.food.position()
+        marg=10
+        if abs(pos[0]-x)<=marg and abs(pos[1]-y)<=marg:
+            print("......... the snake found food")
+            return(True)
+        else:
+            return(False)
+    
+    def hit_the_wall(self, pos):
+        if pos[0]<=-self.set_width/2+10 or pos[0]>=self.set_width/2-10 or pos[1]<=-self.set_height/2 or pos[1]>=self.set_height/2:
+            return(True)
+        else:
+            return(False)
+
+
+
+
+
+
+   
+    
